576e2a325834236b623667e08e2268ef362ff425
IsraelHub <israelcunhaolive@gmail.com>
2025-09-02 21:13:26 -0300

    feat: implementação de entidades e repositório de gêneros

---
+++ api-rest/src/generos/generos.repository.ts
@@ -0,0 +1,20 @@
+import { Genero } from './genero.entity';
+
+export const generos: Genero[] = [
+  {
+    id: 1,
+    nome: 'Drama',
+  },
+  {
+    id: 2,
+    nome: 'Comédia',
+  },
+  {
+    id: 3,
+    nome: 'Ação',
+  },
+  {
+    id: 4,
+    nome: 'Terror',
+  },
+];
